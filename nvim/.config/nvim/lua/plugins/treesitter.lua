dc948aa3ad1b8addb7fbcdb078d5226788bea0be
Igor Maurício <igor.mauricio.eng@gmail.com>
2026-02-06 09:55:56 -0300

    Feat: fixes in treesitter and lsp config

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,19 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  config = function()
-    require("nvim-treesitter.install").prefer_git = true
-  end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require("nvim-treesitter.install").prefer_git = true
+    end,
+    opts = {
+      highlight = { enable = true },
+      indent = { enable = true },
+      ensure_installed = {
+        "vue",
+        "html",
+        "javascript",
+        "typescript",
+        "css",
+      },
+    },
+  },
 }
